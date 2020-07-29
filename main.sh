47a200c6ac0079a23860240e703723487f79d12c
tyler71 <sagsdf_replit@xyzz.work>
2020-07-29 13:41:13 +0000

    add in manual replit for github imports

--- main.sh
+++ main.sh
@@ -15,16 +15,16 @@ DEV=true
 # URL=https://example.com
 URL="https://${REPL_SLUG}.${REPL_OWNER}.repl.co"
 
-
-source ./config/setup/install_deps.sh
-source ./config/setup/init.sh
-
 if [ "$USE_EXTENDED" = true ] \
 && [ -d site ]; then
     cd site
     export PATH="$PATH:$(npm bin)"
     cd -
 fi
+
+source ./config/setup/install_deps.sh
+source ./config/setup/init.sh
+
 
 if [ "$DEV" = true ]; then
   (
